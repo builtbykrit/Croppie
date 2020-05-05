2a57816fe35ff7905fbe7080eaafe25ce01e8b2a
Austin Carr <austin.carr@builtbykrit.com>
2020-05-05 10:03:38 -0400

    Patches rotation function to set height and width to new height that happened after rotation.

--- croppie.js
+++ croppie.js
@@ -177,7 +177,7 @@
     /* Utilities */
     function loadImage(src, doExif) {
         if (!src) { throw 'Source image missing'; }
-        
+
         var img = new Image();
         img.style.opacity = '0';
         return new Promise(function (resolve, reject) {
@@ -1471,13 +1471,15 @@
 
         self.data.orientation = getExifOffset(self.data.orientation, deg);
         drawCanvas(canvas, self.elements.img, self.data.orientation);
+        self._originalImageWidth = canvas.width;
+        self._originalImageHeight = canvas.height;
         _updateCenterPoint.call(self, true);
         _updateZoomLimits.call(self);
 
         // Reverses image dimensions if the degrees of rotation is not divisible by 180.
         if ((Math.abs(deg) / 90) % 2 === 1) {
-            let oldHeight = self._originalImageHeight;
-            let oldWidth = self._originalImageWidth;
+            var oldHeight = self._originalImageHeight;
+            var oldWidth = self._originalImageWidth;
             self._originalImageWidth = oldHeight;
             self._originalImageHeight = oldWidth;
         }
